48086284fe3e9cfe60c17d7f9f0c5e3350f86eed
Erik Hansson <hansson_erik@outlook.com>
2024-12-09 06:39:42 +0100

    feat: validates team and trainer name

--- modules/domain/src/battle.test.ts
+++ modules/domain/src/battle.test.ts
@@ -1,11 +1,84 @@
-import { battle } from './battle'
+import { battle, Team, Pokemon } from './battle'
+
+export const validPokemon: (overrides?: Partial<Pokemon>) => Pokemon = ({
+	pokedexId = 1,
+	name = 'Testimon',
+	multipliers = [],
+	types = ['Fire'],
+	weaknesses = [],
+} = {}) => ({
+	name,
+	multipliers,
+	pokedexId,
+	types,
+	weaknesses,
+})
+
+export const validTeam: () => Team = () => ({
+	trainer: { name: 'testTrainer' },
+	pokemons: [validPokemon(), validPokemon(), validPokemon()],
+})
 
 describe('given a new battle', () => {
-	describe('when checking if game is completed', () => {
-		test('then it should return false', async () => {
+	test('then game should not be completed', async () => {
+		const testBattle = battle()
+
+		expect(testBattle.gameCompleted).toBe(false)
+	})
+
+	test('then no errors should exist', async () => {
+		const testBattle = battle()
+
+		expect(testBattle.hasError).toBe(false)
+	})
+
+	describe('when adding a valid homeTeam', () => {
+		test('no errors should exist', async () => {
 			const testBattle = battle()
 
-			expect(testBattle.gameCompleted).toBe(false)
+			testBattle.addHomeTeam(validTeam())
+
+			expect(testBattle.hasError).toBe(false)
 		})
+	})
+
+	describe('when adding a invalid home team', () => {
+		test.each`
+			invalidTeam                                         | expectedError
+			${''}                                               | ${'Team must be a object'}
+			${' '}                                              | ${'Team must be a object'}
+			${'      '}                                         | ${'Team must be a object'}
+			${undefined}                                        | ${'Team must be a object'}
+			${null}                                             | ${'Team must be a object'}
+			${true}                                             | ${'Team must be a object'}
+			${false}                                            | ${'Team must be a object'}
+			${1}                                                | ${'Team must be a object'}
+			${0}                                                | ${'Team must be a object'}
+			${Symbol()}                                         | ${'Team must be a object'}
+			${new Error('')}                                    | ${'Team must be a object'}
+			${{}}                                               | ${'Trainer must be a object'}
+			${{ ...validTeam(), trainer: null }}                | ${'Trainer must be a object'}
+			${{ ...validTeam(), trainer: undefined }}           | ${'Trainer must be a object'}
+			${{ ...validTeam(), trainer: 'invalid' }}           | ${'Trainer must be a object'}
+			${{ ...validTeam(), trainer: '' }}                  | ${'Trainer must be a object'}
+			${{ ...validTeam(), trainer: { name: '' } }}        | ${'Trainer name must be a non empty string'}
+			${{ ...validTeam(), trainer: { name: ' ' } }}       | ${'Trainer name must be a non empty string'}
+			${{ ...validTeam(), trainer: { name: null } }}      | ${'Trainer name must be a non empty string'}
+			${{ ...validTeam(), trainer: { name: undefined } }} | ${'Trainer name must be a non empty string'}
+			${{ ...validTeam(), trainer: { name: 1 } }}         | ${'Trainer name must be a non empty string'}
+		`(
+			'then "$invalidTeam" should cause error with message $expectedError',
+			async ({ invalidTeam, expectedError }) => {
+				const testBattle = battle()
+
+				testBattle.addHomeTeam(invalidTeam)
+
+				expect(testBattle.hasError).toBe(true)
+				expect(testBattle.errors).toContainEqual({
+					message: expectedError,
+					type: 'validation',
+				})
+			}
+		)
 	})
 })
--- modules/domain/src/battle.ts
+++ modules/domain/src/battle.ts
@@ -1,14 +1,120 @@
-interface Team {
+export const battle = () => {
+	const startHealth = 1000
+	const battleState: BattleState = { gameCompleted: false }
+	const errors: BattleError[] = []
+	const hasError = () => errors.length > 0
+	const pushError = (e: BattleError) => errors.push(e)
+
+	return {
+		addHomeTeam(team: Team) {
+			if (!isValidTeam(team, { onError: pushError })) {
+				return
+			}
+			const homeTeam = {
+				trainer: {
+					name: team.trainer.name,
+				},
+				pokemons: team.pokemons.map(pokemon => {
+					const validatedPokedexId = pokidexIdMustBeNumberFrom1to151({
+						pokedexId: pokemon.pokedexId,
+						onError: pushError,
+					})
+					return {
+						pokedexId: validatedPokedexId,
+						name: pokemon.name,
+						types: pokemon.types,
+						weaknesses: pokemon.weaknesses,
+						multipliers: pokemon.multipliers,
+						health: startHealth,
+					}
+				}),
+			}
+
+			if (!hasError()) {
+				battleState.homeTeam = homeTeam
+			}
+		},
+		get gameCompleted() {
+			return battleState.gameCompleted
+		},
+		get hasError() {
+			return hasError()
+		},
+		get errors() {
+			return [...errors.map(e => ({ ...e }))]
+		},
+	}
+}
+
+const isPartial = <T extends object>(val: unknown | T): val is Partial<T> =>
+	typeof val === 'object' && val !== null && !(val instanceof Error)
+
+const isValidTeam = (
+	team: unknown,
+	{
+		onError,
+	}: {
+		onError?: (e: BattleError) => void
+	} = {}
+): team is Team => {
+	if (!isPartial<Team>(team)) {
+		onError?.({ type: 'validation', message: 'Team must be a object' })
+		return false
+	}
+
+	if (!isPartial(team.trainer)) {
+		onError?.({ type: 'validation', message: 'Trainer must be a object' })
+		return false
+	}
+
+	if (!isNonEmptyTrimmedString(team.trainer.name)) {
+		onError?.({
+			type: 'validation',
+			message: 'Trainer name must be a non empty string',
+		})
+		return false
+	}
+
+	return true
+}
+const isNonEmptyTrimmedString = <T>(val: unknown | T): val is string =>
+	typeof val === 'string' && val.trim().length > 0
+
+const pokidexIdMustBeNumberFrom1to151 = <T>({
+	pokedexId,
+	onError,
+}: {
+	pokedexId: T
+	onError: (e: BattleError) => void
+}) => {
+	const isNumberFrom1to151String =
+		typeof pokedexId === 'number' && pokedexId <= 151 && pokedexId >= 1
+
+	if (!isNumberFrom1to151String) {
+		onError({
+			type: 'validation',
+			message: 'PokidexId must be a number from 1 to 151',
+		})
+	}
+
+	return pokedexId
+}
+
+export interface BattleError {
+	type: 'validation'
+	message: string
+}
+export interface Team {
 	trainer: Trainer
 	pokemons: Pokemon[]
 }
 
-interface Pokemon {
+export interface Pokemon {
 	name: string
 	pokedexId: number
 	multipliers: number[]
 	weaknesses: string[]
-	type: string[]
+	types: string[]
 }
 
 interface Trainer {
@@ -23,14 +129,4 @@ interface BattleState {
 	homeTeam?: { trainer?: Trainer; pokemons?: BattleActivePokemon[] }
 	awayTeam?: { trainer?: Trainer; pokemons?: BattleActivePokemon[] }
 	gameCompleted: boolean
-}
-
-export const battle = () => {
-	const battleState: BattleState = { gameCompleted: false }
-
-	return {
-		get gameCompleted() {
-			return battleState.gameCompleted
-		},
-	}
 }
