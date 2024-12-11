adc71704bdc3142482246d2f3f2f745182c4268a
Erik Hansson <hansson_erik@outlook.com>
2024-12-11 07:09:27 +0100

    build: setup express server and api/battle route

---
+++ interfaces/rest-api/src/app.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+import { createBattleHandler } from './handlers/battleRequestHandler'
+import { errorHandler } from './middlewares/errorHandler'
+
+const app = express()
+
+app.use(express.json())
+app.use('/api/battle', createBattleHandler)
+app.use(errorHandler)
+
+export default app
--- interfaces/rest-api/src/server.ts
+++ interfaces/rest-api/src/server.ts
@@ -1,1 +1,5 @@
-console.log('server entry points')
+import app from './app'
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => console.info(`Server running on port ${PORT}`))
