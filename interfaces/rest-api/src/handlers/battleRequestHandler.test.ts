0dc8c52de5e98e0fbbc8b0c96e5bb5ddebe4ab9f
Erik Hansson <hansson_erik@outlook.com>
2024-12-11 07:13:43 +0100

    feat: implement handler for POST /api/battle

--- interfaces/rest-api/src/handlers/battleRequestHandler.test.ts
+++ interfaces/rest-api/src/handlers/battleRequestHandler.test.ts
@@ -1,3 +1,223 @@
-describe('battleRequestHandler', () => {
-	it('tests should run', () => expect(true).toBe(true))
+import { createBattleHandler } from './battleRequestHandler'
+
+const mockedRes = {
+	status: jest.fn().mockReturnThis(),
+	json: jest.fn(),
+}
+const mockedNext = jest.fn()
+
+beforeEach(() => {
+	mockedRes.status.mockReturnThis()
+	mockedRes.json.mockReset()
+})
+
+describe('createBattleHandler', () => {
+	test('success', async () => {
+		const mockedReq = {
+			body: { homeTeam: validTeam('Erik'), awayTeam: validTeam('Johan') },
+		}
+
+		await createBattleHandler(mockedReq as any, mockedRes as any, mockedNext)
+
+		expect(mockedRes.status).toHaveBeenCalledWith(201)
+		expect(mockedRes.json).toHaveBeenCalledWith({
+			success: true,
+			data: expect.any(Object),
+		})
+	})
+})
+
+describe('invalid payload', () => {
+	test.each`
+		invalid             | testDescription               | expectedError
+		${undefined}        | ${'undefined'}                | ${'Expected an object, but received: undefined'}
+		${null}             | ${'null'}                     | ${'Expected an object, but received: null'}
+		${0}                | ${'0'}                        | ${'Expected an object, but received: 0'}
+		${[]}               | ${'empty array'}              | ${'Expected an object, but received: '}
+		${[1]}              | ${'one items number array'}   | ${'Expected an object, but received: 1'}
+		${[1, 2]}           | ${'two items number array'}   | ${'Expected an object, but received: 1,2'}
+		${[1, 2, 3]}        | ${'three items number array'} | ${'Expected an object, but received: 1,2,3'}
+		${''}               | ${'emptyString'}              | ${'Expected an object, but received: \"\"'}
+		${'text'}           | ${'"text" string'}            | ${'Expected an object, but received: \"text\"'}
+		${Number.MAX_VALUE} | ${'big number'}               | ${'Expected an object, but received: 1.7976931348623157e+308'}
+	`(
+		'[body] "$testDescription" should result in a 400 (Bad Request)',
+		async ({ invalid, expectedError }) => {
+			const mockedReq = {
+				body: invalid,
+			}
+			await createBattleHandler(mockedReq as any, mockedRes as any, mockedNext)
+
+			expect(mockedRes.status).toHaveBeenCalledWith(400)
+			expect(mockedRes.json).toHaveBeenCalledWith({
+				message: expectedError,
+				success: false,
+			})
+		}
+	)
+
+	test.each`
+		invalid             | testDescription               | expectedError
+		${undefined}        | ${'undefined'}                | ${'At path: awayTeam.trainerId -- Expected a string, but received: undefined'}
+		${null}             | ${'null'}                     | ${'At path: awayTeam.trainerId -- Expected a string, but received: null'}
+		${0}                | ${'0'}                        | ${'At path: awayTeam.trainerId -- Expected a string, but received: 0'}
+		${[]}               | ${'empty array'}              | ${'At path: awayTeam.trainerId -- Expected a string, but received: '}
+		${[1]}              | ${'one items number array'}   | ${'At path: awayTeam.trainerId -- Expected a string, but received: 1'}
+		${[1, 2]}           | ${'two items number array'}   | ${'At path: awayTeam.trainerId -- Expected a string, but received: 1,2'}
+		${[1, 2, 3]}        | ${'three items number array'} | ${'At path: awayTeam.trainerId -- Expected a string, but received: 1,2,3'}
+		${Number.MAX_VALUE} | ${'big number'}               | ${'At path: awayTeam.trainerId -- Expected a string, but received: 1.7976931348623157e+308'}
+		${[]}               | ${'empty pokemons array'}     | ${'At path: awayTeam.trainerId -- Expected a string, but received: '}
+	`(
+		'[awayTeam.trainerId] "$testDescription" should result in a 400 (Bad Request)"',
+		async ({ invalid, expectedError }) => {
+			mockedRes.json.mockReset()
+			const mockedReq = {
+				body: {
+					awayTeam: { ...validTeam('Erik'), trainerId: invalid },
+					homeTeam: validTeam('Johan'),
+				},
+			}
+			await createBattleHandler(mockedReq as any, mockedRes as any, mockedNext)
+
+			expect(mockedRes.status).toHaveBeenCalledWith(400)
+			expect(mockedRes.json).toHaveBeenCalledWith({
+				message: expectedError,
+				success: false,
+			})
+		}
+	)
+
+	test.each`
+		invalid              | testDescription                          | expectedError
+		${undefined}         | ${'undefined'}                           | ${'At path: awayTeam -- Expected an object, but received: undefined'}
+		${null}              | ${'null'}                                | ${'At path: awayTeam -- Expected an object, but received: null'}
+		${0}                 | ${'0'}                                   | ${'At path: awayTeam -- Expected an object, but received: 0'}
+		${[]}                | ${'empty array'}                         | ${'At path: awayTeam -- Expected an object, but received: '}
+		${[1]}               | ${'one items number array'}              | ${'At path: awayTeam -- Expected an object, but received: 1'}
+		${[1, 2]}            | ${'two items number array'}              | ${'At path: awayTeam -- Expected an object, but received: 1,2'}
+		${[1, 2, 'invalid']} | ${'three items number and string array'} | ${'At path: awayTeam -- Expected an object, but received: 1,2,invalid'}
+		${''}                | ${'emptyString'}                         | ${'At path: awayTeam -- Expected an object, but received: ""'}
+		${'text'}            | ${'"text" string'}                       | ${'At path: awayTeam -- Expected an object, but received: "text"'}
+		${Number.MAX_VALUE}  | ${'big number'}                          | ${'At path: awayTeam -- Expected an object, but received: 1.7976931348623157e+308'}
+		${[]}                | ${'empty pokemons array'}                | ${'At path: awayTeam -- Expected an object, but received: '}
+		${[]}                | ${'empty pokemons array'}                | ${'At path: awayTeam -- Expected an object, but received: '}
+	`(
+		'[awayTeam] "$testDescription" should result in a 400 (Bad Request) with message',
+		async ({ invalid, expectedError }) => {
+			const mockedReq = {
+				body: {
+					awayTeam: invalid,
+					homeTeam: validTeam('Johan'),
+				},
+			}
+			await createBattleHandler(mockedReq as any, mockedRes as any, mockedNext)
+
+			expect(mockedRes.status).toHaveBeenCalledWith(400)
+			expect(mockedRes.json).toHaveBeenCalledWith({
+				message: expectedError,
+				success: false,
+			})
+		}
+	)
+
+	test.each`
+		invalid             | testDescription               | expectedError
+		${undefined}        | ${'undefined'}                | ${'At path: awayTeam.pokemons -- Expected an array value, but received: undefined'}
+		${null}             | ${'null'}                     | ${'At path: awayTeam.pokemons -- Expected an array value, but received: null'}
+		${0}                | ${'0'}                        | ${'At path: awayTeam.pokemons -- Expected an array value, but received: 0'}
+		${[]}               | ${'empty array'}              | ${'At path: awayTeam.pokemons -- Expected a array with a length of `3` but received one with a length of `0`'}
+		${[1]}              | ${'one items number array'}   | ${'At path: awayTeam.pokemons -- Expected a array with a length of `3` but received one with a length of `1`'}
+		${[1, 2]}           | ${'two items number array'}   | ${'At path: awayTeam.pokemons -- Expected a array with a length of `3` but received one with a length of `2`'}
+		${[1, 2, 'hej']}    | ${'three items number array'} | ${'At path: awayTeam.pokemons.2 -- Expected a number, but received: "hej"'}
+		${Number.MAX_VALUE} | ${'big number'}               | ${'At path: awayTeam.pokemons -- Expected an array value, but received: 1.7976931348623157e+308'}
+		${[]}               | ${'empty pokemons array'}     | ${'At path: awayTeam.pokemons -- Expected a array with a length of `3` but received one with a length of `0`'}
+	`(
+		'[awayTeam.pokemons] "$testDescription" should result in a 400 (Bad Request)',
+		async ({ invalid, expectedError }) => {
+			const mockedReq = {
+				body: {
+					awayTeam: { ...validTeam('Erik'), pokemons: invalid },
+					homeTeam: validTeam('Johan'),
+				},
+			}
+			await createBattleHandler(mockedReq as any, mockedRes as any, mockedNext)
+
+			expect(mockedRes.status).toHaveBeenCalledWith(400)
+			expect(mockedRes.json).toHaveBeenCalledWith({
+				message: expectedError,
+				success: false,
+			})
+		}
+	)
+
+	test.each`
+		invalid              | testDescription                          | expectedError
+		${undefined}         | ${'undefined'}                           | ${'At path: homeTeam -- Expected an object, but received: undefined'}
+		${null}              | ${'null'}                                | ${'At path: homeTeam -- Expected an object, but received: null'}
+		${0}                 | ${'0'}                                   | ${'At path: homeTeam -- Expected an object, but received: 0'}
+		${[]}                | ${'empty array'}                         | ${'At path: homeTeam -- Expected an object, but received: '}
+		${[1]}               | ${'one items number array'}              | ${'At path: homeTeam -- Expected an object, but received: 1'}
+		${[1, 2]}            | ${'two items number array'}              | ${'At path: homeTeam -- Expected an object, but received: 1,2'}
+		${[1, 2, 'invalid']} | ${'three items number and string array'} | ${'At path: homeTeam -- Expected an object, but received: 1,2,invalid'}
+		${''}                | ${'emptyString'}                         | ${'At path: homeTeam -- Expected an object, but received: ""'}
+		${'text'}            | ${'"text" string'}                       | ${'At path: homeTeam -- Expected an object, but received: "text"'}
+		${Number.MAX_VALUE}  | ${'big number'}                          | ${'At path: homeTeam -- Expected an object, but received: 1.7976931348623157e+308'}
+		${[]}                | ${'empty pokemons array'}                | ${'At path: homeTeam -- Expected an object, but received: '}
+		${[]}                | ${'empty pokemons array'}                | ${'At path: homeTeam -- Expected an object, but received: '}
+	`(
+		'[homeTeam] "$testDescription" should result in a 400 (Bad Request)',
+		async ({ invalid, expectedError }) => {
+			mockedRes.status.mockReturnThis()
+			mockedRes.json.mockReset()
+			const mockedReq = {
+				body: {
+					homeTeam: invalid,
+					awayTeam: validTeam('Johan'),
+				},
+			}
+
+			await createBattleHandler(mockedReq as any, mockedRes as any, mockedNext)
+
+			expect(mockedRes.status).toHaveBeenCalledWith(400)
+			expect(mockedRes.json).toHaveBeenCalledWith({
+				message: expectedError,
+				success: false,
+			})
+		}
+	)
+
+	test.each`
+		invalid             | testDescription             | expectedError
+		${undefined}        | ${'undefined'}              | ${'At path: homeTeam.pokemons -- Expected an array value, but received: undefined'}
+		${null}             | ${'null'}                   | ${'At path: homeTeam.pokemons -- Expected an array value, but received: null'}
+		${0}                | ${'0'}                      | ${'At path: homeTeam.pokemons -- Expected an array value, but received: 0'}
+		${[]}               | ${'empty array'}            | ${'At path: homeTeam.pokemons -- Expected a array with a length of `3` but received one with a length of `0`'}
+		${[1]}              | ${'one items number array'} | ${'At path: homeTeam.pokemons -- Expected a array with a length of `3` but received one with a length of `1`'}
+		${[1, 2]}           | ${'two items number array'} | ${'At path: homeTeam.pokemons -- Expected a array with a length of `3` but received one with a length of `2`'}
+		${Number.MAX_VALUE} | ${'big number'}             | ${'At path: homeTeam.pokemons -- Expected an array value, but received: 1.7976931348623157e+308'}
+	`(
+		'[homeTeam.pokemons] "$testDescription" result in a 400 (Bad Request) with message',
+		async ({ invalid, expectedError }) => {
+			mockedRes.status.mockReturnThis()
+			mockedRes.json.mockReset()
+			const mockedReq = {
+				body: {
+					homeTeam: { ...validTeam('Erik'), pokemons: invalid },
+					awayTeam: validTeam('Johan'),
+				},
+			}
+			await createBattleHandler(mockedReq as any, mockedRes as any, mockedNext)
+
+			expect(mockedRes.status).toHaveBeenCalledWith(400)
+			expect(mockedRes.json).toHaveBeenCalledWith({
+				message: expectedError,
+				success: false,
+			})
+		}
+	)
+})
+
+const validTeam = (name = 'trainerName') => ({
+	trainerId: name,
+	pokemons: [1, 2, 3],
 })
--- interfaces/rest-api/src/handlers/battleRequestHandler.ts
+++ interfaces/rest-api/src/handlers/battleRequestHandler.ts
@@ -1,1 +1,49 @@
-export const battleRequestHandler = () => {}
+import { array, Describe, number, string, type, size } from 'superstruct'
+import {
+	createBattleSimulationHandler,
+	CreateBattleSimulationCommand,
+} from '@snyggt/pokemon-battle-app/src/createBattleSimulation'
+import { inMemoryPokemonService } from '@snyggt/pokemon-battle-infra/src/inMemoryPokemonService'
+import { Response, Request, NextFunction } from 'express'
+
+const handleCreateBattleSimulationCommand = createBattleSimulationHandler({
+	pokemonService: inMemoryPokemonService,
+})
+
+export const createBattleHandler = async (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	try {
+		const [error, validCommand] = bodySchema.validate(req.body)
+
+		if (!validCommand) {
+			res.status(400).json({
+				message: error.message,
+				success: false,
+			})
+			return
+		}
+
+		const battleResponse =
+			await handleCreateBattleSimulationCommand(validCommand)
+
+		res.status(201).json({ success: true, data: battleResponse })
+	} catch (error) {
+		next(error)
+	}
+}
+
+const pokemons = size(array(number()), 3)
+
+const bodySchema: Describe<CreateBattleSimulationCommand> = type({
+	awayTeam: type({
+		trainerId: string(),
+		pokemons: pokemons,
+	}),
+	homeTeam: type({
+		trainerId: string(),
+		pokemons: pokemons,
+	}),
+})
